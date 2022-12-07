2ae817407a702a7dc7b4d7445239eedfe5ecdfd8
Rodrigo do Vale <rodrigodovale2002@gmail.com>
2022-12-06 21:32:14 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+//document (para acessar todo o html) querrySelector(para procurar algum elemento dentro do document(html))//
+//o ""." é usado para acessar os dados possiveis no elemento//
+document.querySelectorAll('.tecla')
+
