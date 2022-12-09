f4250124e8d2105f23bdf9f305dd8fb0ac7bffbb
Rodrigo do Vale <rodrigodovale2002@gmail.com>
2022-12-08 21:44:23 -0300

    finalização projeto

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    }
+    else{
+        console.log('elemento não encontrado ou seletor inválido');
+    }
 }
+
 
 //document (para acessar todo o html) querrySelector(para procurar algum elemento dentro do document(html))//
 //o ""." é usado para acessar os dados possiveis no elemento//
-document.querySelectorAll('.tecla')
 
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+for(let contador = 0; contador < listaDeTeclas.length;contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    //console.log(idAudio)
+
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+    };
+
+    //colocando o efeito ao usar o teclado de mudar o fundo para vermelho   
+    //evento para selecionar a forma de chamar as teclas do teclado
+    tecla.onkeydown = function(evento){
+        if(evento.code == 'Space' || evento.code == 'Enter'){
+            tecla.classList.add('ativa')
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+    }
+    //console.log(contador);
+}
